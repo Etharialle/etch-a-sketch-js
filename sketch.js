2dd1f4a5542084b3fb266ce69b2abb25bcfcb50e
etharialle <christopher.m.adams2@gmail.com>
2024-05-02 00:13:19 -0700

    removed commented out test lines

--- sketch.js
+++ sketch.js
@@ -1,12 +1,8 @@
 
 function createGrid() {
     const gridSize = 16;
-    //let divColumn;
-    //let divRow;
     const container = document.querySelector("#container");
-    let b = 1;
     for (let i = 1; i <= gridSize; i++) {
-        //let divColumn = i;
         const divColumn = document.createElement("div");
         divColumn.classList.add("no-padding");
         for (let j = 1; j <= gridSize; j++) {
@@ -14,13 +10,10 @@ function createGrid() {
             divRow.style.opacity = 0;
             divRow.className = "grid-box";
             divRow.id = i + j;
-            
             const divGridLines = document.createElement("div");
             divGridLines.className = "grid-borders";
-            
             divColumn.appendChild(divGridLines);
             divGridLines.appendChild(divRow);
-            
         }
         container.appendChild(divColumn);
     }
